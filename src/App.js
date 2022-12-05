10b6a2c54792bdcb94eb0636a6dfc7b72cfe08b2
aaron <you@example.com>
2022-12-05 16:53:50 +0000

    profile starting

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "./scss/app.css";
 //import Home from "./components/Home";
-import Home2 from "./user/pages/Home2";
-
+//import Home2 from "./user/Home2";
+import Profile from "./user/Profile";
 // import Login from "./components/Login";
 //import Register from "../components/Register";
 //import Counter from "./components/Counter";
@@ -15,8 +15,8 @@ function App() {
      
      
   
-   <Home2></Home2>
    
+   <Profile></Profile>
     
     
     </div>
---
+++ src/user/Profile.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+
+const Profile = () => {
+    return (
+        <div className="container">
+<div className="home-head">
+    <div className="logo">
+        <h2>the</h2>
+        <h1>portal</h1>
+    </div>
+    <div className="user-id">
+        <p>Welcome:place holder</p>
+        
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+<div className="logout-container">
+    <div className="logout-text">
+    <p>log out</p>
+</div>
+</div>
+
+
+</div>
+ 
+    )
+}
+
+export default Profile
