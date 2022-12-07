d58c800047439a6856abc2094f121b9bde91cc10
aaron <you@example.com>
2022-12-07 22:35:16 +0000

    profile page update card

--- src/user/Profile.jsx
+++ src/user/Profile.jsx
@@ -3,19 +3,64 @@ import React from "react";
 
 const Profile = () => {
     return (
-        <div className="container">
-<div className="home-head">
-    <div className="logo">
+        <div className="profile-container">
+<div className="profile-home-head">
+   
+   
+   <div className="back-arrow">
+    <div className="img-container">
+
+    </div>
+   </div>
+ <div className="profile-logo">
         <h2>the</h2>
         <h1>portal</h1>
     </div>
-    <div className="user-id">
-        <p>Welcome:place holder</p>
-        
+
+</div>
+
+<div className="profile-card">
+
+<div className="profile-img-pat-details"> 
+
+<div className="profile-img-container">
+    <div className="profile-img">
+
     </div>
 </div>
 
+<div className="profile-name-patnumber">
+    <p>name:test</p>
+    <p>surname:test</p>
+    <p>patient num:1234</p>
+</div>
 
+</div>
+
+<div className="profile-ailment">
+    <p>ailment: <br /> a rash of red, itchy spots that turn
+into fluid-filled blisters They then crust over to
+form scabs which eventually drop off.</p>
+</div>
+
+<div className="profile-treatment">
+    <p>treatment: soda cream </p>
+</div>
+
+<div className="profile-doctor">
+    <p>Doctor in charge of treatment : <br /> DR Brown </p>
+<p>WARD: 13B</p>
+</div>
+
+<div className="Appointments-due">
+
+    <p>Appointments due</p>
+    <p>Check up :25/09/2022 14.00pm </p>
+</div>
+
+
+
+</div>
 
 
 
