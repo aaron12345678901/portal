47d26fb6c8e82c4cbb89a05982b58de188140c2a
aaron <you@example.com>
2022-12-14 21:10:45 +0000

    routing update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import "./scss/app.css";
 //import Home from "./components/Home";
-//import Home2 from "./user/Home2";
+import Home2 from "./user/Home2";
 import Profile from "./user/Profile";
 // import Login from "./components/Login";
 //import Register from "../components/Register";
 //import Counter from "./components/Counter";
-//import Entertainment from "./user/Entertainement";
+import Entertainment from "./user/Entertainement";
 
 
 function App() {
   return (
     <div className="App">
      
-     
-  
-   <Profile></Profile>
+     <BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home2 />}>
+					<Route
+						index
+						element={<Home2 />}
+					/>
+					<Route
+						path="Profile"
+						element={
+							<Profile />
+						}
+					/>
+					<Route
+						path="Entertainement"
+						element={
+							<Entertainment />
+						}
+					/>
+					
+				</Route>
+			</Routes>
+		</BrowserRouter>
   
     
     
--- src/user/Home2.jsx
+++ src/user/Home2.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {Outlet, Link } from 'react-router-dom';
 
 
 const Home2 = () => {
@@ -16,13 +17,15 @@ const Home2 = () => {
 </div>
 
 <div className="h-firstline-links">
+
+<Link to="Profile">
     <div className="hfirst-link">
         <div className="hfirst-img"></div>
         <div className="hfirst-link-text">
             <p>profile</p>
         </div>
     </div>
-    
+    </Link>
     <div className="hsecond-link">
         <div className="hsecond-img"></div>
         <div className="hsecond-link-text">
@@ -32,18 +35,25 @@ const Home2 = () => {
     </div>  
 
     <div className="h-third-links">
+    
         <div className="hthird-link">
             <div className="hthird-img">
+         </div>
 
-            </div>
+        
         </div>
+        
         <div className="hthird-line-text">
             <p>entertainment</p>
         </div>
+        
     </div>
 
 
+
+ <Outlet />
 </div>
+
  
     )
 }
